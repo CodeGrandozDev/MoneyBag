f333b024ec7501f2afe5c8978e5095245ba0374d
CodeMajorGeek <37310498+CodeMajorGeek@users.noreply.github.com>
2018-11-11 20:09:19 +0100

    adding command money

--- src/fr/CodeGrandoz/MoneyBag/MoneyBag.java
+++ src/fr/CodeGrandoz/MoneyBag/MoneyBag.java
@@ -2,6 +2,8 @@ package fr.CodeGrandoz.MoneyBag;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.CodeGrandoz.MoneyBag.commands.CommandMoney;
+
 public class MoneyBag extends JavaPlugin{
 	
 	@Override
@@ -23,6 +25,8 @@ public class MoneyBag extends JavaPlugin{
 	}
 	
 	private void init() {
+		
+		this.getCommand("money").setExecutor(new CommandMoney());
 		
 		System.out.println("Le plugin MoneyBag est initialiser !");
 	}
